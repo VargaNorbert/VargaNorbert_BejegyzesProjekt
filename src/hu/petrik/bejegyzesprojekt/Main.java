4371021ce8b32b15eaa8588f8487406d9e4e5664
Varga Norbert <vnorbi02@gmail.com>
2022-09-29 09:19:33 +0200

    Bejegyzes osztály -tulajdonságok felvétele -konstruktor létrehozása -getter/setter metódusok -like() eljárás -toString() metódus
    Main
    -lista felvétele
    -objektumok litához adása konzolról
    -beolvasás fájlból

--- src/hu/petrik/bejegyzesprojekt/Main.java
+++ src/hu/petrik/bejegyzesprojekt/Main.java
@@ -1,8 +1,14 @@
 package hu.petrik.bejegyzesprojekt;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
+
 
 public class Main {
 
@@ -10,40 +16,63 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Bejegyzes b1 = new Bejegyzes("Varga Norbert", "Megbukom");
+        Bejegyzes b2 = new Bejegyzes("Gipsz Jakab", "Teszt");
+
+        bejegyzesek.add(b1);
+        bejegyzesek.add(b2);
+
         try {
             bejegyzesFelveteleKonzolrol();
-        } catch (Exception e) {
+        } catch (InputMismatchException e) {
             System.out.println("Nem megfelelő példány!");
         }
 
-
-
-
+        try {
+            beolvas("bejegyzesek.csv");
+        } catch (FileNotFoundException e) {
+            System.out.println("Hiba a beolvasásnál");
+        } catch (IOException e) {
+            System.out.println("Ismeretlen hiba");
+        }
 
 
     }
 
+    public static void beolvas(String fajlNev) throws IOException {
+        FileReader fr = new FileReader(fajlNev);
+        BufferedReader br = new BufferedReader(fr);
+        String sor = br.readLine();
+        while (sor != null && !sor.equals("")) {
+            String[] adatok = sor.split(";");
+            Bejegyzes bejegyzes = new Bejegyzes(adatok[0], adatok[1]);
 
+            bejegyzesek.add(bejegyzes);
+            sor = br.readLine();
 
+        }
+        fr.close();
+        br.close();
+    }
 
 
     private static void bejegyzesFelveteleKonzolrol() {
-    Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("Hány elemet szeretnél felvenni?");
         int a = sc.nextInt();
         sc.nextLine();
 
-        for(int i = 0; i<a;i++) {
-        System.out.printf("%d. elem  szerzője?",i+1);
-        String szerzo = sc.nextLine();
+        for (int i = 0; i < a; i++) {
+            System.out.printf("%d. elem  szerzője?", i + 1);
+            String szerzo = sc.nextLine();
 
-        System.out.printf("%d. elem tartalma?",i+1);
-        String tartalom = sc.nextLine();
+            System.out.printf("%d. elem tartalma?", i + 1);
+            String tartalom = sc.nextLine();
 
-        Bejegyzes bejegyzes= new Bejegyzes(szerzo,tartalom);
+            Bejegyzes bejegyzes = new Bejegyzes(szerzo, tartalom);
 
-        bejegyzesek.add(bejegyzes);
+            bejegyzesek.add(bejegyzes);
 
         }
 
