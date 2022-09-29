6a130092c1735ae20cbd6bfb6761b8c7d28f6167
Varga Norbert <vnorbi02@gmail.com>
2022-09-29 09:06:16 +0200

    Bejegyzes osztály -tulajdonságok felvétele -konstruktor létrehozása -getter/setter metódusok -like() eljárás -toString() metódus
    Main
    -lista felvétele
    -objektumok litához adása konzolról

--- src/hu/petrik/bejegyzesprojekt/Main.java
+++ src/hu/petrik/bejegyzesprojekt/Main.java
@@ -1,7 +1,51 @@
 package hu.petrik.bejegyzesprojekt;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+
+    static List<Bejegyzes> bejegyzesek = new ArrayList<>();
+
     public static void main(String[] args) {
+
+        try {
+            bejegyzesFelveteleKonzolrol();
+        } catch (Exception e) {
+            System.out.println("Nem megfelelő példány!");
+        }
+
+
+
+
+
+
+    }
+
+
+
+
+
+    private static void bejegyzesFelveteleKonzolrol() {
+    Scanner sc = new Scanner(System.in);
+
+        System.out.println("Hány elemet szeretnél felvenni?");
+        int a = sc.nextInt();
+        sc.nextLine();
+
+        for(int i = 0; i<a;i++) {
+        System.out.printf("%d. elem  szerzője?",i+1);
+        String szerzo = sc.nextLine();
+
+        System.out.printf("%d. elem tartalma?",i+1);
+        String tartalom = sc.nextLine();
+
+        Bejegyzes bejegyzes= new Bejegyzes(szerzo,tartalom);
+
+        bejegyzesek.add(bejegyzes);
+
+        }
 
     }
 }
